bc330387dfd817bd4ad390965de1a8d2e1453cea
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-11-30 19:54:05 +0300

    End Task4

---
+++ Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли четным (делится на 2 без остатка)
+
+Console.Write("Enter int N = ");
+int N = int.Parse(Console.ReadLine());
+
+int a = 0;
+double b = 0;
+
+a = N%10;
+b = N/10;
+
+Console.Write("Last number");
+Console.WriteLine(a);
+Console.Write("Previous Numbers" + " " + b);
+
