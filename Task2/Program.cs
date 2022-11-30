700dbb13ab2c793e65efc59a06ac1a9f1354f3e8
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-11-30 18:12:29 +0300

    Enter Numbers

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿//Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+
+Console.Write("Enter Number a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter Number b = ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("Max");
+Console.WriteLine(a);
+
+
+}
+else (a < b)
+{
+    Console.Write("Max");
+Console.WriteLine(b);
+
+}
