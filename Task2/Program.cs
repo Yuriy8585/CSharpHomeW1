475ce8e21efb6396649de673aae755256e9497e0
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-11-30 18:31:52 +0300

    Result

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,20 +3,19 @@
 // a = 2; b = 10 -> max = 10
 // a = -9; b = -3 -> max = -3
 
+int Max(int a, int b) => a > b ? a : b;
+int Max(int a, int b) => Max(Max(a, b), c);
+
 Console.Write("Enter Number a = ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Enter Number b = ");
 int b = int.Parse(Console.ReadLine());
-if (a > b)
+if (a = b)
 {
-    Console.Write("Max");
-Console.WriteLine(a);
-
+    Console.Write("Result YES");
 
 }
-else (a < b)
+else
 {
-    Console.Write("Max");
-Console.WriteLine(b);
-
+    Console.Write("Result NO");
 }
