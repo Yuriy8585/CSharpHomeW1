9c6815e60704e61f9d8e372c043e0ce14005812a
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-11-30 19:49:02 +0300

    End Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+// 2 , 3 , 7
+
+ {
+        int[] arr = { 2, 3, 7, -1, 2 };
+ 
+        Console.WriteLine("Minimum number is " + arr.Min());
+        Console.WriteLine("Maximum number is " + arr.Max());
+    }
