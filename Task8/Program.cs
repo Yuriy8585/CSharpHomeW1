59b0a552f5db4d6020a35c4131f3c6039376776e
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-11-30 20:29:44 +0300

    End Task8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напиите программу которая на вход принимает одно число N, а выводит значения все целые числа от -N до N
+
+Console.Write("Enter int N = ");
+int N = int.Parse(Console.ReadLine());
+int a = 2;
+while (a < N+1)
+{
+ Console.Write(" " + a + " ");
+ a = a + 2;
+}
+
+ if (N % 2 == 0)
+ 
+ {
+    Console.Write(" ДА Введенное число является четным");
+ 
+    Console.Read();
+
+ }
