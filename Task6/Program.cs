ea812f0484dc2f3fb10aacedc5c5894dd420f8d0
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-11-30 20:08:49 +0300

    End Task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,15 +1,30 @@
 ﻿// Напишите программу, которая на вход принимает число и выдает, является ли четным (делится на 2 без остатка)
 
-Console.Write("Enter int N = ");
-int N = int.Parse(Console.ReadLine());
 
-int a = 0;
-double b = 0;
-
-a = N%10;
-b = N/10;
-
-Console.Write("Last number");
-Console.WriteLine(a);
-Console.Write("Previous Numbers" + " " + b);
-
+{
+ 
+ int i;
+ 
+ Console.Write("Введите число : ");
+ 
+ i = int.Parse(Console.ReadLine());
+ 
+ if (i % 2 == 0)
+ 
+ {
+ 
+    Console.Write("Введенное число является четным");
+ 
+    Console.Read();
+ 
+ }
+ 
+ else
+ 
+ {
+ 
+    Console.Write("Введенное число нечетное");
+ 
+    Console.Read();
+ }
+ }
